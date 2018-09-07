4a7f15c2be1fe50b9ea54f35af15d9cba8fac00b
Devansh Maurya <Devansh233@gmail.com>
2018-09-07 23:01:24 +0530

    Added initial sample splash screen

--- app/src/main/java/in/cognitia/cognitia18/SplashScreenActivity.java
+++ app/src/main/java/in/cognitia/cognitia18/SplashScreenActivity.java
@@ -1,13 +1,30 @@
 package in.cognitia.cognitia18;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreenActivity extends Activity {
+
+    //Splash Screen Timer
+    private static int SPLASH_TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            //This method will be executed once the wait is over
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+
+                //Close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
