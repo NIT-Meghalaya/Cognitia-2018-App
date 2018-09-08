e0764844efe9a24c6ee7af39f1889bd05ada5ba0
Devansh Maurya <Devansh233@gmail.com>
2018-09-08 07:15:32 +0530

    [INTRO SLIDER] Added sample layouts and images (WARN: WelcomeActivity.java still incomplete)

---
+++ app/src/main/java/in/cognitia/cognitia18/WelcomeActivity.java
@@ -0,0 +1,60 @@
+package in.cognitia.cognitia18;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class WelcomeActivity extends Activity {
+
+    private ViewPager viewPager;
+    private MyViewPagerAdapter myViewPagerAdapter;
+    private LinearLayout dotsLayout;
+    private TextView[] dots;
+    private int[] layouts;
+    private Button btnSkip, btnNext;
+    private PreferenceManager preferenceManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Check for first time launch
+        preferenceManager = new PreferenceManager(this);
+        if (!preferenceManager.isFirstTimeLaunch()) {
+            launchHomeScreen();
+            finish();
+        }
+
+        //Make notification bar transparent
+        getWindow().getDecorView().setSystemUiVisibility( View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+
+        setContentView(R.layout.activity_welcome);
+
+        viewPager = (ViewPager) findViewById(R.id.view_pager);
+        dotsLayout = (LinearLayout) findViewById(R.id.layoutDots);
+        btnSkip = (Button) findViewById(R.id.btn_skip);
+        btnNext = (Button) findViewById(R.id.btn_next);
+
+        //Layout for all welcome sliders
+        layouts = new int[] {
+                R.layout.welcome_slide1,
+                R.layout.welcome_slide2,
+                R.layout.welcome_slide3,
+                R.layout.welcome_slide4 };
+
+        //Adding bottom dots
+        addBottomDots(0);
+
+        //Making notification bar transparent
+        changeStatusBarColor();
+
+        myViewPagerAdapter = new MyViewpagetAdapter();
+        viewPager.setAdapter(myViewPagerAdapter);
+        viewPager.addOnPageChangeListener(viewPagerpageChangeListener);
+
+    }
+}
