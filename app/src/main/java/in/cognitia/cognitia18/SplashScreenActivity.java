41c3ef4f4341e84c667ba6e0deda3c1f4b8936f7
ichimichi <ichimichilaribok@gmail.com>
2018-09-09 02:22:07 +0530

    [SPLASHCREEN] improved preloader animation speed and duration of 3s

--- app/src/main/java/in/cognitia/cognitia18/SplashScreenActivity.java
+++ app/src/main/java/in/cognitia/cognitia18/SplashScreenActivity.java
@@ -8,7 +8,7 @@ import android.os.Handler;
 public class SplashScreenActivity extends Activity {
 
     //Splash Screen Timer
-    private static int SPLASH_TIME_OUT = 3000;
+    private static int SPLASH_TIME_OUT = 2990;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
