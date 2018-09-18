bb0ed1856cfcd2be54c0eef2cc6874abf6749200
Devansh Maurya <Devansh233@gmail.com>
2018-09-18 18:25:38 +0530

    [CognitiaEvent] Updated CognitiaEvent class

--- app/src/main/java/in/cognitia/cognitia18/CognitiaEvent.java
+++ app/src/main/java/in/cognitia/cognitia18/CognitiaEvent.java
@@ -6,6 +6,13 @@ package in.cognitia.cognitia18;
 
 public class CognitiaEvent extends EventCategory{
     private String shortDescription;
+    private String name;
+    private String description;
+    private String about;
+    private String rules;
+    private String robotSpecs;
+    private String teamGuidelines;
+    private String coordinators;
 
     public CognitiaEvent(String name, String shortDescription, int imageResId) {
         super(name, imageResId, 0);
@@ -18,5 +25,61 @@ public class CognitiaEvent extends EventCategory{
 
     public String getShortDescription() {
         return shortDescription;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setAbout(String about) {
+        this.about = about;
+    }
+
+    public String getAbout() {
+        return about;
+    }
+
+    public void setRules(String rules) {
+        this.rules = rules;
+    }
+
+    public String getRules() {
+        return rules;
+    }
+
+    public void setRobotSpecs(String robotSpecs) {
+        this.robotSpecs = robotSpecs;
+    }
+
+    public String getRobotSpecs() {
+        return robotSpecs;
+    }
+
+    public void setTeamGuidelines(String teamGuidelines) {
+        this.teamGuidelines = teamGuidelines;
+    }
+
+    public String getTeamGuidelines() {
+        return teamGuidelines;
+    }
+
+    public void setCoordinators(String coordinators) {
+        this.coordinators = coordinators;
+    }
+
+    public String getCoordinators() {
+        return coordinators;
     }
 }
