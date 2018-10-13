6fcd920ced6611f853d69ce20642a5cf00d6b7a0
Devansh Maurya <Devansh233@gmail.com>
2018-10-13 06:11:26 +0530

    Associated departmental event images to data

--- app/src/main/java/in/cognitia/cognitia18/Util/EventsImagesAssociator.java
+++ app/src/main/java/in/cognitia/cognitia18/Util/EventsImagesAssociator.java
@@ -62,6 +62,30 @@ public class EventsImagesAssociator {
         imageMap.put(ROBODICTION, drawable.robodiction);
         imageMap.put(ROBO_BRIDGE, drawable.robobridge);
         imageMap.put(RACE_AGAINST_TIME, drawable.race_against_time);
+
+        imageMap.put("Palace of Cards", drawable.palace_of_cards);
+        imageMap.put("Android Bridge Constructor Simulator", drawable.android_bridge_simulator);
+        imageMap.put("Autocad Drafting", drawable.autocad_drafting);
+        imageMap.put("Bascule Bridge", drawable.bascule_bridge);
+        imageMap.put("Cantilever Design", drawable.cantilever_design);
+
+        imageMap.put("Beat the Q", drawable.beat_the_q);
+        imageMap.put("Bicode", drawable.bicode);
+        imageMap.put("Crack it", drawable.crack_it);
+        imageMap.put("Megabyte", drawable.megabyte);
+
+        imageMap.put("Electromania", drawable.electromania);
+        imageMap.put("Fast and Furious", drawable.fast_and_furious);
+        imageMap.put("Inventolution", drawable.inventolution);
+
+        imageMap.put("Not So Fast", drawable.not_so_fast);
+        imageMap.put("The Ultimate Swot", drawable.the_ultimate_swot);
+        imageMap.put("Tricky Circuits", drawable.tricky_circuits);
+
+        imageMap.put("Assembler", drawable.assembler);
+        imageMap.put("Inertia", drawable.inertia);
+        imageMap.put("Load Upholder", drawable.load_upholder);
+
     }
 
 
