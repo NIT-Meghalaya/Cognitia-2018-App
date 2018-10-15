b42914685dcfa447cfded927d1ccc8cf684ecc98
ichimichi <ichimichilaribok@gmail.com>
2018-10-15 19:04:08 +0530

    [INTROSLIDER] updated layouts with correct messages

--- app/src/main/java/in/cognitia/cognitia18/WelcomeActivity.java
+++ app/src/main/java/in/cognitia/cognitia18/WelcomeActivity.java
@@ -8,7 +8,6 @@ import android.support.annotation.NonNull;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.text.Html;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
